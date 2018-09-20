41be5280121b7e1b4bce5aa942a1b162ba95f0e9
Thet Hnin Aye <thaye.thethnin@gmail.com>
2018-09-21 04:41:49 +0800

    Uploading Week 7 Practical

---
+++ Programming Exercises/Exercise 1/Book.java
@@ -0,0 +1,17 @@
+public abstract class Book {
+    String title = new String();
+    double price;
+    public Book(String t)
+    {
+        title = t;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+    public abstract void setPrice();
+}
